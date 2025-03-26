67fa9959a5e42014b4932b29378cdc6730933e82
unknown <239180@TECH.WYB.AC.LK>
2025-03-26 20:32:25 +0530

    second commit

---
+++ q4.py
@@ -0,0 +1,8 @@
+num=10
+if(num>5):
+    print("num greater than 5")
+elif (num>10):
+    print("num geater than")
+    
+else:
+    print("num smaller than 5")
---
+++ q6.py
@@ -0,0 +1,11 @@
+x=int(input("enter number 1: "))
+y=int(input("enter number 2: "))
+z=int(input("enter number 3: "))
+
+if (x < y):
+    print("number 1 is bigger")
+elif (x < z):
+    print("number 3 is bigger")
+else:
+    print("number 2 is bigger")
+   
